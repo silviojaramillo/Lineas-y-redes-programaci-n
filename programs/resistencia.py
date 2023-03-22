c416cc7a8acd195d668a773a95f680f21aa26166
Alex Jaramillo <91851947+silviojaramillo@users.noreply.github.com>
2023-03-22 12:49:18 -0500

    Update

--- programs/resistencia.py
+++ programs/resistencia.py
@@ -12,7 +12,7 @@ temperatureRef = float(input('Ingrese la temperatura de referencia: '))
 temperatureMax = float(input('Ingrese el valor de la temperatura máxima: '))
 
 # Recopilando el valor de Tk
-temperatureTk = float(input('Ingrese el valor de Tk: '))
+temperatureTk = float(input('Ingrese el valor de la temperatura característica Tk: '))
 
 # Recopilando el valor de la frecuencia
 frequency = float(input('Ingrese el valor de la frecuencia expresado en Hz: '))
@@ -23,6 +23,9 @@ relativePermeability = float(input('Ingrese el valor de la permeabilidad relativ
 # Recopilando el valor de n
 n = int(input('Ingrese el valor de n: '))
 
+# Recopilando el valor de la distancia de la línea
+distance = float(input('Ingrese la distancia de la línea en km: '))
+
 #############################################################################################
 #                                     Área de cálculos                                      #
 #############################################################################################
@@ -32,8 +35,12 @@ c = (2*math.pi*frequency*relativePermeability)/(resistorDC*10**4)
 # Calculando el valor de Rac
 resistorAC = resistorDC*(1+c**2/12 -c**4/180)
 
+# Calculando la resistencia por fase
 racfase = resistorAC/n
 
-# Mostrando el resultado
-print(f"El valor de la resistencia en AC es {racfase}")
+# Calculando la resistencia total por la longitud de la línea
+rACTotal = racfase*distance
+
+# Mostrando el resultado 
+print(f"El valor de la resistencia en AC a {temperatureMax} grados por fase es {racfase} Ω/km y la resistencia total debido a la longitud de la línea de {distance} es {rACTotal} Ω")
 
