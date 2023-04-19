ec42862e9b68eaaef27fb3040e818f08bf688358
Alex Jaramillo <91851947+silviojaramillo@users.noreply.github.com>
2023-04-19 01:21:36 -0500

    Update

--- programs/referir_temperatura.py
+++ programs/referir_temperatura.py
@@ -3,7 +3,7 @@ import os
 os.system('cls')
 
 # Recopilando los valores de la resistencia
-resistor1 = float(input('Ingrese el valor de la resistencia en DC: '))
+resistor1 = float(input('Ingrese el valor de la resistencia en DC en Ω/km: '))
 
 # Recopilando la temperatura de la resistencia DC
 temperature1 = float(input('Ingrese el valor de la temperatura en grados para la que Rdc es igual a: {} Ohms: '.format(resistor1)))
@@ -17,4 +17,4 @@ temperature_k = float(input('Ingrese el valor de la temperatura característica 
 temperature_end = resistor1*((temperature_k + temperature2)/(temperature_k + temperature1))
 
 # Mostrando el valor de la temperatura final
-print(f"El valor de la resistencia DC ({resistor1}) referida a {temperature2} es {round(temperature_end,5)} Ohms")
+print(f"El valor de la resistencia DC ({resistor1}) referida a {temperature2} grados es {round(temperature_end,5)} Ω/km")
--- programs/resistencia.py
+++ programs/resistencia.py
@@ -7,14 +7,6 @@ os.system('cls')
 # Recopilando los valores de la resistencia en dc
 resistorDC = float(input('Ingrese el valor de la Resistencia en corriente continua (DC): '))
 
-# Recopilando la temperatura de referencia
-temperatureRef = float(input('Ingrese la temperatura de referencia: '))
-
-# Recopilando el valor de la temperatura máxima
-temperatureMax = float(input('Ingrese el valor de la temperatura máxima: '))
-
-# Recopilando el valor de Tk
-temperatureTk = float(input('Ingrese el valor de la temperatura característica Tk: '))
 
 # Recopilando el valor de la frecuencia
 frequency = float(input('Ingrese el valor de la frecuencia expresado en Hz: '))
@@ -22,11 +14,6 @@ frequency = float(input('Ingrese el valor de la frecuencia expresado en Hz: '))
 #Recopilando el valor de permeabilidad relativa del material conductor
 relativePermeability = float(input('Ingrese el valor de la permeabilidad relativa del material conductor: '))
 
-# Recopilando el valor de n
-n = int(input('Ingrese el valor de n: '))
-
-# Recopilando el valor de la distancia de la línea
-distance = float(input('Ingrese la distancia de la línea en km: '))
 
 #############################################################################################
 #                                     Área de cálculos                                      #
@@ -37,12 +24,7 @@ c = (2*math.pi*frequency*relativePermeability)/(resistorDC*10**4)
 # Calculando el valor de Rac
 resistorAC = resistorDC*(1+c**2/12 -c**4/180)
 
-# Calculando la resistencia por fase
-racfase = resistorAC/n
-
-# Calculando la resistencia total por la longitud de la línea
-rACTotal = racfase*distance
 
 # Mostrando el resultado 
-print(f"El valor de la resistencia en AC a {temperatureMax} grados por fase es {racfase} Ω/km y la resistencia total debido a la longitud de la línea de {distance} es {rACTotal} Ω")
+print(f"El valor de la resistencia en AC es {round(resistorAC,6)} Ω/km")
 
