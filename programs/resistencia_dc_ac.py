38ff638346052f08ca07545c90c69c9b1643eac2
Alex Jaramillo <91851947+silviojaramillo@users.noreply.github.com>
2023-05-07 20:29:22 -0500

    Update

--- programs/resistencia_dc_ac.py
+++ programs/resistencia_dc_ac.py
@@ -13,6 +13,12 @@ frequency = float(input('Ingrese el valor de la frecuencia expresado en Hz: '))
 #Recopilando el valor de permeabilidad relativa del material conductor
 relativePermeability = float(input('Ingrese el valor de la permeabilidad relativa del material conductor: '))
 
+# Recopilando la cantidad de conductores de la línea
+conductors = int(input('Ingrese la cantidad de subconductores por línea: '))
+
+# Recopilando la distancia en km
+distance = float(input('Ingrese la longitud de la línea en km: '))
+
 #############################################################################################
 #                                     Área de cálculos                                      #
 #############################################################################################
@@ -22,6 +28,11 @@ c = (2*math.pi*frequency*relativePermeability)/(resistorDC*10**4)
 # Calculando el valor de Rac
 resistorAC = resistorDC*(1+c**2/12 -c**4/180)
 
+# Calculando la resistencia de la línea
+resistor_total = resistorAC*distance
+
+# Calculando la resistencia por fase
+rFase = resistor_total/conductors
 
 # Mostrando el resultado 
-print(f"El valor de la resistencia en AC es {round(resistorAC,6)} Ω/km")
+print(f"El valor de la resistencia en AC por fase es de {round(rFase,6)} Ω/km")
